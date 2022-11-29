669257a9981363bca085bde909b2fbd40563ec10
Eniola Codes <eniolacodes1@gmail.com>
2022-11-29 20:49:02 +0100

    changes

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -6,7 +6,7 @@ import classes from './MainNavigation.module.css';
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-        <div className={classes.logo}>Quotes</div>
+        <div className={classes.logo}>QuoteR</div>
         <nav className={classes.nav}>
             <ul>
                 <li>
